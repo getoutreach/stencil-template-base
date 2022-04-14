1f0c064cd1afbdb0af9604d2291a206a60af537d
Jared Allard <jared.allard@outreach.io>
2022-04-14 12:12:58 -0700

    move test around, update snapshots

--- templates/main_test.go
+++ templates/main_test.go
@@ -11,8 +11,3 @@ func TestGoMod(t *testing.T) {
 	st := stenciltest.New(t, "go.mod.tpl")
 	st.Run(false)
 }
-
-func TestMainTest(t *testing.T) {
-	st := stenciltest.New(t, "main_test.go.tpl")
-	st.Run(false)
-}
